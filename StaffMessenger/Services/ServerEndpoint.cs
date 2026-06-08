9eee19ea464581acec5dab58c1af66b24830f8d5
droid-darkangel <67116649+droid-darkangel@users.noreply.github.com>
2026-06-08 19:59:10 +0300

    v0.1.2

--- StaffMessenger/Services/ServerEndpoint.cs
+++ StaffMessenger/Services/ServerEndpoint.cs
@@ -2,7 +2,7 @@ namespace StaffMessenger.Services;
 
 public static class ServerEndpoint
 {
-    public const string ApiBaseUrl = "http://72.56.235.188:5072";
+    public const string ApiBaseUrl = "http://5.42.114.194:8000";
 
     public static Uri ApiBaseUri { get; } = new(ApiBaseUrl);
 }
